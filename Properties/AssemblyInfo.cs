d2b62abe92e9c1bf2a047f086f7ca80793e84ddc
christofur <pop.chris@gmail.com>
2016-11-30 07:32:31 +0000

    Structure

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("sitecore_snippets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sitecore_snippets")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("96EAE638-EE2F-4753-82EE-D40FF9649D36")]
---
+++ README.md
@@ -0,0 +1,8 @@
+# Sitecore Snippets
+
+[![Sitecore](http://i.imgur.com/6yD6QAc.png)](http://www.sitecore.net/)
+
+This repository is a collection of useful Sitecore code snippets collected during projects, user-groups, and encountered through [Sitecore Stack Exchange](https://sitecore.stackexchange.com/) and the [Sitecore Forums](https://community.sitecore.net/)
+
+  - Pull requests are welcome
+  - This repository covers Sitecore versions 7.0+
